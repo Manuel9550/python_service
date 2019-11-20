9fb58b570797b5cd9460751ba68d0e1307dd9d3c
Manuel Poppe Richter <manuel9550@hotmail.com>
2019-11-20 16:53:28 -0500

    :white_check_mark: Add zero month test and fix spelling mistakes

--- python_service/loan/loan.py
+++ python_service/loan/loan.py
@@ -3,18 +3,20 @@ ERROR_ZERO_MONTHS = "Number of Months must be greater than 0"
 
 def calculate_car_loan_by_month(principal_amount, interest_rate, number_of_months):
     rate = interest_rate / 1200
-    monthly_payments = 0
+    final_result = 0
 
 
     # if the interest amount is 0, then use a modified version of the equation so that we don't get a divide by 0 error
-    if interest_rate == 0:
-        monthly_payments = round(principal_amount / number_of_months ,2)
+    if number_of_months == 0:
+        final_result = ERROR_ZERO_MONTHS
+    elif interest_rate == 0:
+        final_result = round(principal_amount / number_of_months ,2)
     else:
-        monthly_payments = round(((rate + (rate / ( ((1 + rate)**number_of_months) -1))) * principal_amount),2)
+        final_result = round(((rate + (rate / ( ((1 + rate)**number_of_months) -1))) * principal_amount),2)
     
-    return monthly_payments
+    return final_result
 
-def calcualte_car_loans(principal_amount, interest_rate):
+def calculate_car_loans(principal_amount, interest_rate):
 
     # make sure the value received is rounded to the nearest two decimal places
     principal_amount_rounded = round(principal_amount, 2)
--- python_service/test/test_loan.py
+++ python_service/test/test_loan.py
@@ -1,6 +1,7 @@
 import unittest
 from python_service.loan.loan import calculate_car_loan_by_month
-from python_service.loan.loan import calcualte_car_loans
+from python_service.loan.loan import calculate_car_loans
+from python_service.loan.loan import ERROR_ZERO_MONTHS
 
 class ValidationTest(unittest.TestCase):
 
@@ -12,6 +13,9 @@ class ValidationTest(unittest.TestCase):
 
     def test_handles_0_principle_amount_correctly(self):
         self.assertEqual(0, calculate_car_loan_by_month(0,12,10))
+
+    def test_handles_0_months_correctly(self):
+        self.assertEqual(ERROR_ZERO_MONTHS, calculate_car_loan_by_month(30000,60,0))
 
 if __name__ == '__main__':
     unittest.main()
