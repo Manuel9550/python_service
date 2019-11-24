1bf28daed97e7afa63817300face72db8725f4d8
Manuel Poppe Richter <manuel9550@hotmail.com>
2019-11-24 14:45:35 -0500

    :sparkles: option to start service as a client now (where it will connect to the messaging service)

--- python_service/__main__.py
+++ python_service/__main__.py
@@ -1,4 +1,21 @@
+import sys
 from python_service.app import run
+from python_service.client import runClient
 
 if __name__ == '__main__':
-    run()
+    if  len(sys.argv) == 3 :
+
+        # check that a valid ip and port were given
+        port = 0
+        ip = sys.argv[1]
+        port = 0
+      
+        try:
+            if (1 <= int(sys.argv[2]) and int(sys.argv[2]) <= 65535):
+                runClient(ip, int(sys.argv[2]))
+            else:
+                raise ValueError
+        except ValueError:
+            print(f"{sys.argv[2]} is NOT a VALID port number.")   
+    else:
+        run()
---
+++ python_service/client.py
@@ -0,0 +1,10 @@
+import socket   
+from python_service.loan.loan import calculate_car_loans
+from python_service.connection_handling.connection_handling import handle_connected_client
+
+def runClient(ip, port):
+    
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip, port))
+        handle_connected_client(s)  
+
